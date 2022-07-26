3dd1f8727fc963b6a1c4586e0806449eb799ab1c
VitalySlabada <vitaly.slabada@gmail.com>
2022-07-25 22:05:35 -0500

    update - security done, minor bugs left

--- src/main/java/com/example/accountingapp/repository/CategoryRepository.java
+++ src/main/java/com/example/accountingapp/repository/CategoryRepository.java
@@ -1,13 +1,16 @@
 package com.example.accountingapp.repository;
 
 import com.example.accountingapp.entity.Category;
+import com.example.accountingapp.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByCompany(Company companyByLoggedInUser);
 
 //    List<Category> findAllBy();
 
