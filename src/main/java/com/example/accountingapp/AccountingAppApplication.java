4617200851df93465bd4a1e93632bdc4fbaa1a48
VitalySlabada <vitaly.slabada@gmail.com>
2022-08-07 10:30:16 -0500

    security repeat , next step -> modify thymeleaf login

--- src/main/java/com/example/accountingapp/AccountingAppApplication.java
+++ src/main/java/com/example/accountingapp/AccountingAppApplication.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class AccountingAppApplication {
-    @Value("${stripe.api.key}")
-    private String stripeApiKey;
 
     public static void main(String[] args) {
         SpringApplication.run(AccountingAppApplication.class, args);
@@ -22,9 +22,11 @@ public class AccountingAppApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
-    @PostConstruct
-    public void setup() {
-        Stripe.apiKey = stripeApiKey;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
 
 }
